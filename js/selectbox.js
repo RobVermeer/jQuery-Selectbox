239152356991136ec56d22bfb3fa560dbde4d2eb
Rob Vermeer <rob@vermeertech.nl>
2014-08-21 21:21:27 +0200

    Fix bug

--- js/selectbox.js
+++ js/selectbox.js
@@ -12,44 +12,42 @@
 			callback: function(){}
 		};
 		var opts = $.extend(defaults, options);
-		var selectbox = 1;
 		
 		$('html').click(function() {
 			$(".selectbox").find('ul').hide();
 		});
 		
 		return this.each(function(){
-			var $this = $(this);
-			$this.hide();
-			$this.attr('id', 'sb_' + selectbox);
-			$this.before('<div class="selectbox sb_' + selectbox + '"><span>' + $this.children().first().text() + '</span></div>');
-			$(".sb_" + selectbox).data('sb', selectbox).append('<ul></ul>');
+			var $this = $(this),
+				selectbox = $this.attr('id');
+			//$this.hide();
+			$this.before('<div class="selectbox ' + selectbox + '"><span>' + $this.children().first().text() + '</span></div>');
+			$("." + selectbox).data('sb', selectbox).append('<ul></ul>');
 			$($this).find('option').each( function() {
-				$(".sb_" + selectbox + ' ul').append('<li>' + $(this).text() + '</li>').hide();
+				$("." + selectbox + ' ul').append('<li>' + $(this).text() + '</li>').hide();
 			});
 			
 			$($this).find('option:selected').each( function() {
-				$(".sb_" + selectbox + ' span').text($(this).text());
+				$("." + selectbox + ' span').text($(this).text());
 			});
 
 			
-			$(".sb_" + selectbox).click(function(e) {
+			$("." + selectbox).click(function(e) {
 				$(this).find('ul').toggle();
 				e.stopPropagation();
 			});
 			
-			$(".sb_" + selectbox + ' li').click(function() {
-				var $this = $(this);
-				var sb_id = $this.parent().parent().data('sb');
-				var this_val = $this.html();
+			$("." + selectbox + ' li').on('click', function() {
+				var $this = $(this),
+					sb_id = $this.parent().parent().data('sb'),
+					this_val = $this.html();
 				$this.parent().parent().find('span').text(this_val);
-				$('#sb_' + sb_id + ' option[selected]').removeAttr("selected");
-				$('#sb_' + sb_id).children().filter(function() {
+				$('#' + sb_id + ' option[selected]').removeAttr("selected");
+				$('#' + sb_id).children().filter(function() {
 					return $(this).text() == this_val;
 				}).attr("selected", "selected");
-				opts.callback.call(this);
+				opts.callback.call(undefined, $('#' + sb_id + ' option[selected]'));
 			});
-			selectbox++;
 		});
 	};
 })(jQuery);
